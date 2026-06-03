bfd1f8d58cace4160f680038d4e0a28621f3a2d2
Jacob Nguyen <jacobnguyen.dev@gmail.com>
2026-06-03 16:15:41 +0900

    Cargo init and create module files

---
+++ src/analyzer.rs
@@ -0,0 +1,1 @@
+//lindera and grammar parser stuff
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+// ui and other stuff and load the other 2 modules  
+// the stuff below is random generated garbage from cargo
+
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
