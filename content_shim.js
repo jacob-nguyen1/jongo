12bde78484fa31eb132e12e3dfa64869059e1715
Jacob Nguyen <jacobnguyen.dev@gmail.com>
2026-07-15 15:33:10 +0900

    Revert "nate's stuff"
    This reverts commit 8775ec7e52fb1b8227fce8c654dc2ad319b4d87c.

--- content_shim.js
+++ content_shim.js
@@ -1,20 +1,9 @@
 (async () => {
   try {
-    const { enabled = true } = await chrome.storage.local.get("enabled");
-
-    const src = chrome.runtime.getURL("pkg/jongo.js");
-    const { default: init, content_start, set_enabled } = await import(src);
-    await init({ module_or_path: chrome.runtime.getURL("pkg/jongo_bg.wasm") });
-
-    // Always start; gate inside Rust
+    const src = chrome.runtime.getURL('pkg/jongo.js');
+    const { default: init, content_start } = await import(src);
+    await init({ module_or_path: chrome.runtime.getURL('pkg/jongo_bg.wasm') });
     content_start();
-    if (!enabled) set_enabled(false);
-
-    chrome.storage.onChanged.addListener((changes, area) => {
-      if (area === "local" && changes.enabled !== undefined) {
-        set_enabled(changes.enabled.newValue ?? true);
-      }
-    });
   } catch (e) {
     console.error("Jongo:", e);
   }
